5c388acf6c963e81aa8994e75bfe4a84808e1a9b
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2020-03-23 12:43:25 +0300

    second

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,85 +1,104 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import Chart from 'chart.js/dist/Chart.min.js'
-
+import Chart from 'chart.js';
+import Axios from 'axios';
 
 class Charts extends Component {
 //
 constructor(props) {
     super(props);
+
     this.state = { 
-             confirmed:200000,
-             recovery:80000,
-             deaths:10000,
-             confirmedAF:1000,
-             recoveryAF:200,
-             deathsAF:30,
-             confirmedETH:9,
+             confirmed:0,
+             recovery:0,
+             deaths:0,  
+             confirmedETH:0,
              recoveryETH:0,
              deathsETH:0,
-             country:[]
+             lastUpdate:''
+             
      }
 }
 
 async getData (){
-   // const res =await Axios.get("https://covid-19.mathdro.id/api")
-   //const resCountry =await Axios.get("https://covid-19.mathdro.id/api/Countries");
-  // const countrys =Object.keys(resCountry.data.country)
+   const res =await Axios.get("https://covid-19.mathdro.id/api")
+   
    this.setState({
-   /* confirmed:res.data.confirmed.value ,
-    recovery:res.data.recovery.value,
-    deaths:res.data.deaths.value,
-    country:countrys
-    */
+    confirmed:this.numberWithCommas(res.data.confirmed.value ),
+    recovery:this.numberWithCommas(res.data.recovered.value),
+    deaths:this.numberWithCommas(res.data.deaths.value),
    })
+//world
+
+
+const myChartRefworld =this.chartRef2.current.getContext('2d');
+
+   
+  
+/// world data 
+new Chart(myChartRefworld, {
+   
+    type: 'doughnut', //bar,horizontalBar,pie,line,doughnut,radar,polarArea
+    data: {
+        labels: ['deaths', 'confirmed', 'recovered'],
+        datasets: [{
+            label: 'population',
+            data: [res.data.deaths.value,res.data.confirmed.value,res.data.recovered.value],
+            backgroundColor: [' rgb(245, 29, 29)', 'rgb(230, 230, 54)', 'rgb(46, 134, 46)', ],
+
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'World Corona Cases'
+        },
+        legend: {
+            display: true,
+            position: 'right',
+            labels: {
+                fontColor: 'black'
+            }
+        },
+        layout: {
+            padding: {
+                left: 50,
+                right: 0
+            }
+        }
+
+    }
+});
+
+
+
 }
 
-getCountryDataEth =()=> {
-   //const resCountry =await Axios.get(`https://covid-19.mathdro.id/api/Countries/Etiopia`);
+async getCountryDataEth () {
+   const resCountry =await Axios.get(`https://covid-19.mathdro.id/api/countries/Ethiopia`);
+  const updateDates =resCountry.data.lastUpdate
+  
+  const convertedStartDate = new Date(updateDates);
+  const shortForm =convertedStartDate.toLocaleDateString();
+
    this.setState({
-    /* confirmedETH:resCountry.data.confirmed.value ,
-     recoveryETH:resCountry.data.recovery.value,
-     deathsETH:resCountry.data.deaths.value,
-     */
+     confirmedETH:this.numberWithCommas(resCountry.data.confirmed.value) ,
+     recoveryETH:this.numberWithCommas(resCountry.data.recovered.value),
+     deathsETH:this.numberWithCommas(resCountry.data.deaths.value),
+     lastUpdate:shortForm
     })
-}
-    getCountryDataAfrica =()=> {
-        //const resCountry =await Axios.get(`https://covid-19.mathdro.id/api/Countries/Africa`);
-        this.setState({
-         /* confirmedAF:resCountry.data.confirmed.value ,
-          recoveryAF:resCountry.data.recovery.value,
-          deathsAF:resCountry.data.deaths.value,
-          */
-         })
-}
-getCountryDataWorld =()=> {
-    //const resCountry =await Axios.get(`https://covid-19.mathdro.id/api/Countries/world`);
-    this.setState({
-     /* confirmed:resCountry.data.confirmed.value ,
-      recovery:resCountry.data.recovery.value,
-      deaths:resCountry.data.deaths.value,
-      */
-     })
-}
-    //
-chartRef =React.createRef();
-chartRef2 =React.createRef();
-chartRef3 =React.createRef();
+    //charts
 
-componentDidMount() {
-    this.getData();
     const myChartRefeth =this.chartRef.current.getContext('2d');
-    const myChartRefworld =this.chartRef2.current.getContext('2d');
-    const myChartRefafrica =this.chartRef3.current.getContext('2d');
-
-   new Chart(myChartRefeth, {
+    new Chart(myChartRefeth, {
         type: 'horizontalBar', //bar,horizontalBar,pie,line,doughnut,radar,polarArea
         data: {
-            labels: ['death', 'virus positive', 'recovery'],
+            labels: ['deaths', 'confirmed', 'recovered'],
             datasets: [{
                 label: 'population',
-                data: [this.state.deathsETH,this.state.confirmedETH,this.state.recoveryETH],
-                backgroundColor: ['red', 'yellow', 'green', ],
+                data: [resCountry.data.deaths.value,resCountry.data.confirmed.value,resCountry.data.recovered.value],
+                backgroundColor: [' rgb(245, 29, 29)', 'rgb(230, 230, 54)', 'rgb(46, 134, 46)', ],
+
             }]
         },
         options: {
@@ -104,75 +123,25 @@ componentDidMount() {
     
         }
     });
-    /// world data 
-    new Chart(myChartRefworld, {
-        type: 'doughnut', //bar,horizontalBar,pie,line,doughnut,radar,polarArea
-        data: {
-            labels: ['death', 'virus positive', 'recovery'],
-            datasets: [{
-                label: 'population',
-                data: [this.state.deaths,this.state.confirmed,this.state.recovery],
-                backgroundColor: ['red', 'yellow', 'green', ],
-    
-            }]
-        },
-        options: {
-            title: {
-                display: true,
-                text: 'World Corona Cases'
-            },
-            legend: {
-                display: true,
-                position: 'right',
-                labels: {
-                    fontColor: 'black'
-                }
-            },
-            layout: {
-                padding: {
-                    left: 50,
-                    right: 0
-                }
-            }
-    
-        }
-    });
-    // africa data
-    
-    
-new Chart(myChartRefafrica, {
-        type: 'bar', //bar,horizontalBar,pie,line,doughnut,radar,polarArea
-        data: {
-            labels: ['death', 'virus positive', 'recovery'],
-            datasets: [{
-                label: 'population',
-                data: [this.state.deathsAF,this.state.confirmedAF,this.state.recoveryAF],
-                backgroundColor: ['red', 'yellow', 'green', ],
-    
-            }]
-        },
-        options: {
-            title: {
-                display: true,
-                text: 'Africa Corona Cases'
-            },
-            legend: {
-                display: false,
-                position: 'right',
-                labels: {
-                    fontColor: 'black'
-                }
-            },
-            layout: {
-                padding: {
-                    left: 50,
-                    right: 0
-                }
-            }
-    
-        }
-    }); 
+
+
+
 }
+
+
+chartRef =React.createRef();
+chartRef2 =React.createRef();
+
+componentDidMount() {
+    this.getData();
+  
+    this.getCountryDataEth ();
+} 
+
+numberWithCommas(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
 
   render() {  
    
@@ -182,7 +151,7 @@ new Chart(myChartRefafrica, {
     
 
         return ( 
-
+           
        <React.Fragment>
            
       <div className="data_container">
@@ -198,24 +167,15 @@ new Chart(myChartRefafrica, {
         <div className="lst">Deaths: <span>{this.state.deathsETH}</span></div>
         <div className="lst">Confirmed: <span className="postive">{this.state.confirmedETH}</span></div>
         <div className="lst">Recovered <span className="rec">{this.state.recoveryETH}</span></div>
+        <div class="alert alert-success" role="alert">
+        last update:{this.state.lastUpdate}
+</div>
+ 
             </div>
         </div>
-        <div className="data" id="africa">
-            <h1>Coronavirus Cases in Africa</h1>
-            <hr size="2" width="70%" color="green"/>
-            <div className="chart">
-                <canvas id='myChart3'
-                  ref={this.chartRef3}
-                ></canvas>
-            </div>
-            <div className="disc">
-        <div className="lst">Deaths: <span>{this.state.deathsAF}</span></div>
-        <div className="lst">Confirmed: <span className="postive">{this.state.confirmedAF}</span></div>
-        <div className="lst">Recovered <span className="rec">{this.state.recoveryAF}</span></div>
-            </div>
-        </div>
-        <div className="data">
-            <h1>Coronavirus Cases in world</h1>
+      
+        <div className="data" >
+            <h1 id="world">Coronavirus Cases in world</h1>
             <hr size="2" width="70%" color="green"/>
             <div className="chart">
                 <canvas id='myChart2'
@@ -225,9 +185,12 @@ new Chart(myChartRefafrica, {
             <div className="disc">
         <div className="lst">Deaths: <span>{this.state.deaths}</span></div>
         <div className="lst">Confirmed: <span className="postive">{this.state.confirmed}</span></div>
-        <div className="lst">Recovered <span className="rec">{this.state.recovery}</span></div>
+        <div className="lst">Recovered <span className="rec">{this.state.recovery}</span></div>  
+        <div class="alert alert-success" role="alert">
+        last update:{this.state.lastUpdate}
+</div>
             </div>
-
+    
         </div>
         <Link to="/allCountry" className=" center btn btn-info">All Country</Link>
     </div>
@@ -240,7 +203,7 @@ new Chart(myChartRefafrica, {
         
             <Link className="link"to="www.who.int"><big>www.who.int</big></Link>
     </div>
-    <Link class="top" ref={this.mytop}>Top</Link>
+       <a className="top" href="#">Top</a>
        </React.Fragment>
          );
     }
