aec10503fa4d266f310e2d6690e0f26b86a1ddef
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2020-03-23 12:38:55 +0300

    Update allCountry.js

--- src/components/allCountry.js
+++ src/components/allCountry.js
@@ -1,45 +1,72 @@
 import React,{Component} from 'react';
 import './allCountry.css';
+import Axios from 'axios';
 class AllCountry extends Component {
          constructor(props) {
+
                   super(props);
+                  this.getCountryData =this.getCountryData.bind(this);
                   this.state = { 
                            confirmed:0,
                            recovery:0,
                            deaths:0,
-                           country:[]
+                           country:[],
+                           name:'All cases'
+
                    }
          }
          componentDidMount(){
 this.getData();
          }
          async getData (){
-                 // const res =await Axios.get("https://covid-19.mathdro.id/api")
-                 //const resCountry =await Axios.get("https://covid-19.mathdro.id/api/Countries");
-                // const countrys =Object.keys(resCountry.data.country)
+
+                 const res =await Axios.get("https://covid-19.mathdro.id/api")
+                 const resCountry =await Axios.get("https://covid-19.mathdro.id/api/countries");
+                const countrys =Object.keys(resCountry.data.countries)
                  this.setState({
-                 /* confirmed:res.data.confirmed.value ,
-                  recovery:res.data.recovery.value,
+                 confirmed:res.data.confirmed.value ,
+                  recovery:res.data.recovered.value,
                   deaths:res.data.deaths.value,
                   country:countrys
-                  */
+                  
                  })
          }
-         renderCountryOptions(){
+         renderCountryOptions =()=>{
                   return this.state.country.map((country,id)=>{
                   return <option key={id}>{country}</option>
                   })
          }
-         getCountryData =(e)=> {
-                 //const resCountry =await Axios.get(`https://covid-19.mathdro.id/api/Countries/${e.target.value}`);
-                 this.setState({
-                  /* confirmed:resCountry.data.confirmed.value ,
-                   recovery:resCountry.data.recovery.value,
-                   deaths:resCountry.data.deaths.value,
-                   */
-                  })
+        async getCountryData (e) {
+            try{
+              
+                const resCountry =await Axios.get(`https://covid-19.mathdro.id/api/countries/${e.target.value}`);
+              
+                
+            this.setState({
+             confirmed:this.numberWithCommas(resCountry.data.confirmed.value) ,
+              recovery:this.numberWithCommas(resCountry.data.recovered.value),
+              deaths:this.numberWithCommas(resCountry.data.deaths.value),
+              
+             })
+            }
+            catch(err){
+            if(err.response.status === 404)
+            {
+                this.setState({
+                confirmed:"No data avalible " ,
+                recovery:"No data avalible ",
+                deaths:"No data avalible ",
+               
+            })
+            }
+              
+            }
+                
 
          }
+         numberWithCommas(x) {
+            return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        }
          render() { 
                   return ( 
 
@@ -47,26 +74,26 @@ this.getData();
        <select  onChange={this.getCountryData}
        className="custom-select select custom-select-md mb-3">
   <option selected disabled>Search by Country Name</option>
-{this.renderCountryOptions}
+{this.renderCountryOptions()}
 </select>
-        <button className="btn btn-success" type="submit">Search</button>
- <h1 className="name">countryName</h1>
+        <button onClick={this.renderCountryOptions()} className="btn btn-success" type="submit">Search</button>
+                  <h1 className="name">{this.state.name}</h1>
     <div className="country">
 
         <div className="card text-white bg-warning mb-3">
-            <div className="card-header">confirmed</div>
+            <div className="card-header">Confirmed</div>
             <div className="card-body">
                   <h5 className="card-title">{this.state.confirmed}</h5>
             </div>
         </div>
         <div className="card text-white bg-danger mb-3">
-            <div className="card-header">deaths</div>
+            <div className="card-header">Deaths</div>
             <div className="card-body">
                   <h5 className="card-title">{this.state.deaths}</h5>
             </div>
         </div>
         <div className="card text-white bg-success  mb-3">
-            <div className="card-header">recovery</div>
+            <div className="card-header">Recovered</div>
             <div className="card-body">
                   <h5 className="card-title">{this.state.recovery}</h5>
             </div>
